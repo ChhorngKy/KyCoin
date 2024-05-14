31d2b602f8f55a7c9122d5ee6fd01cfaba8f17b3
ChhorngKy <cky2@paragoniu.edu.kh>
2024-05-14 15:17:57 +0700

    Update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const {Blockchain , Transaction } = require("./BlockChain.js")
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
-const myKey = ec.keyFromPrivate('6d4d6cff92d6d9d9d0bf1cf601289f55d2cf9dd2485a7aeb2b7f649e0296e686')
+const myKey = ec.keyFromPrivate('06b9a347c16242c2296c2b0530f552d008e6d66511415d1e4c0002e58db0c9cd')
 const myWalletAddress = myKey.getPublic('hex');
 
 let KyCoin = new Blockchain();
@@ -11,14 +11,11 @@ const tx1 = new Transaction(myWalletAddress, "My Public", 100);
 tx1.signTransaction(myKey);
 KyCoin.createTransaction(tx1);
 
-// console.log("\nStaring Miner...");
-// KyCoin.minePendingTransactions(myWalletAddress);
-// console.log('Balance of Chhorng Ky', KyCoin.getBalanceOfAddress(myWalletAddress))
+console.log("\nStaring Miner...");
+KyCoin.minePendingTransactions(myWalletAddress);
 
-console.log('\nBalance of Chhorng Ky', KyCoin.getBalanceOfAddress(myWalletAddress))
+console.log('Balance of Chhorng Ky', KyCoin.getBalanceOfAddress(myWalletAddress))
 
-// // KyCoin.chain[1].previousHash = "asdf";
+KyCoin.chain[1].previousHash = "asdfsdf";
 
 console.log("Is coin Valid: " + KyCoin.isChainValid().toString());
-
-
